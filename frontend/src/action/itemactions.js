5c72f792e193e6f717446407dc8a7f2b83813f22
divya <106955977+Di12-23@users.noreply.github.com>
2024-07-25 17:10:10 +0530

    Create itemactions.js

---
+++ frontend/src/action/itemactions.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { ITEM_LIST_REQUEST, ITEM_LIST_SUCCESS, ITEM_LIST_FAIL } from '../constants/itemconstants';
+
+export const listItems = () => async (dispatch) => {
+    try {
+        dispatch({ type: ITEM_LIST_REQUEST });
+        const { data } = await axios.get('/api/items');
+        dispatch({ type: ITEM_LIST_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({ type: ITEM_LIST_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message });
+    }
+};
