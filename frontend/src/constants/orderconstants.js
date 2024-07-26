97fd48a1d8a68931b2c6c4c6be9261dfc55710af
divya <106955977+Di12-23@users.noreply.github.com>
2024-07-26 22:20:15 +0530

    orderconstants

---
+++ frontend/src/constants/orderconstants.js
@@ -0,0 +1,4 @@
+// src/constants/orderconstants.js
+export const ORDER_LIST_REQUEST = 'ORDER_LIST_REQUEST';
+export const ORDER_LIST_SUCCESS = 'ORDER_LIST_SUCCESS';
+export const ORDER_LIST_FAIL = 'ORDER_LIST_FAIL';
